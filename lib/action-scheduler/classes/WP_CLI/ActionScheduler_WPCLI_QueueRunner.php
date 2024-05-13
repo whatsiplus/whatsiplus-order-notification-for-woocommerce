ef3344fdbce6b76034af8fc1b2d29cc09c96ff42
Whatsiplus - WhatsApp API <whatsiplus@gmail.com>
2024-05-13 15:38:57 +0330

    up

--- lib/action-scheduler/classes/WP_CLI/ActionScheduler_WPCLI_QueueRunner.php
+++ lib/action-scheduler/classes/WP_CLI/ActionScheduler_WPCLI_QueueRunner.php
@@ -30,7 +30,7 @@ class ActionScheduler_WPCLI_QueueRunner extends ActionScheduler_Abstract_QueueRu
 	public function __construct( ActionScheduler_Store $store = null, ActionScheduler_FatalErrorMonitor $monitor = null, ActionScheduler_QueueCleaner $cleaner = null ) {
 		if ( ! ( defined( 'WP_CLI' ) && WP_CLI ) ) {
 			/* translators: %s php class name */
-			throw new Exception( sprintf( __( 'The %s class can only be run within WP CLI.', 'action-scheduler' ), __CLASS__ ) );
+			throw new Exception( sprintf( esc_html__( 'The %s class can only be run within WP CLI.', 'action-scheduler' ), esc_html( __CLASS__ ) ) );
 		}
 
 		parent::__construct( $store, $monitor, $cleaner );
